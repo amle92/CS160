30be5ec6bac211c16308917d8fb84721243aaa2c
Anthony Tsang <atsang.matterport.com>
2015-11-05 00:02:42 -0800

    Added moocs160_short.sql and Update open2Study.java
    Short DB
    Good Enough open2study.java

--- open2study.java
+++ open2study.java
@@ -7,11 +7,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class open2study {
 
@@ -22,8 +27,9 @@ public class open2study {
      * @throws IllegalAccessException
      * @throws InstantiationException
      * @throws SQLException
+     * @throws ParseException 
      */
-    public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, ParseException {
         //Many things are commented out in this sample program. Uncomment to explore more if needed.
         // Need Jsoup jar files to run this sample program. You may also need to rebuild path, etc.
         // There are many pages that show 15 EDX courses on a webpage as constrained by ?page=some_number.
@@ -77,111 +83,109 @@ public class open2study {
             */
             
             for (int j = 0; j < link.size(); j++) {
-                //Statement statement = connection.createStatement();
+                Statement statement = connection.createStatement();
 
                 String crsurl = "https://www.open2study.com" + link.get(j).attr("href"); //Get the Course Url from href tag and add to www.edx.org to get the full URL to the course
                 System.out.println("crsurl: " + crsurl);
-                String CourseName = crspg.select("h2").get(j).text(); //Get the Course Name from H1 Tag
-                CourseName = CourseName.replace("'", "''");
-                CourseName = CourseName.replace(",", "");
+                
+                /** Course Name **/
+                String CourseName;
+                try {
+	                CourseName = crspg.select("h2").get(j).text(); //Get the Course Name from H1 Tag
+	                CourseName = CourseName.replace("'", "''");
+	                CourseName = CourseName.replace(",", "");
+                } catch (Exception e) {
+                	CourseName = "N/A";
+                }
                 	System.out.println("CourseName: " + CourseName); // DELETE ME
-                String SCrsDesrpTemp = crspg.select("div.adblock_course_body").get(j).text();
-                SCrsDesrpTemp = SCrsDesrpTemp.replace("?", "");
-                //String SCrsDesrp = SCrsDesrpTemp.substring(0, (SCrsDesrpTemp.length()-5)); //To get the course description and remove the extra characters at the end.
-                SCrsDesrpTemp = SCrsDesrpTemp.replace("'", "''");
-                SCrsDesrpTemp = SCrsDesrpTemp.replace(",", "");
+                
+                /** Short Course Description **/
+                String SCrsDesrpTemp;
+                try {
+                	SCrsDesrpTemp= crspg.select("div.adblock_course_body").get(j).text();
+                    SCrsDesrpTemp = SCrsDesrpTemp.replace("?", "");
+                    //String SCrsDesrp = SCrsDesrpTemp.substring(0, (SCrsDesrpTemp.length()-5)); //To get the course description and remove the extra characters at the end.
+                    SCrsDesrpTemp = SCrsDesrpTemp.replace("'", "''");
+                    SCrsDesrpTemp = SCrsDesrpTemp.replace(",", "");
+                } catch (Exception e) {
+                	SCrsDesrpTemp = "N/A";
+                }
                 	System.out.println("SCrsDesrpTemp: " + SCrsDesrpTemp); // DELETE ME
+                
+                /** Course Image **/
                 String CrsImg;
-                /** HTML section to grab image
-                 <figure><img typeof="foaf:Image" class="image-style-course-logo-subjects-block"
-                 src="https://www.open2study.com/sites/default/files/styles/course_logo_subjects_block/public/Course_TIle_agriculture.jpg?itok=tB9Z_fdZ" width="260"
-                 height="140" alt="Agriculture and the World We Live In" title="Agriculture and the World We Live In" /></figure>
-                 */
                 if (a == 0 || a == 1) {
-                    CrsImg = crspg.select("img[].image-style-course-logo-subjects-block").get(j); //Grabs the course image from the img class
+                    CrsImg = crspg.select("img[width=260]").get(j).absUrl("src"); //Grabs the course image from the img class
                 } else {
                     CrsImg = "N/A"; //To get the course image - FOR URL4
                 }
                 	System.out.println("CrsImg: " + CrsImg); // DELETE ME
+                
                 Document crsdoc = Jsoup.connect(crsurl).get();
                 Elements crsheadele = crsdoc.select("section[class=course-header clearfix]");
-                String youtube = crsdoc.select("iframe.media-youtube-player[src]").text(); //Youtube link
-                	System.out.println(youtube); // DELETE ME
+                String youtube;
+                
+                /** Youtube **/
+                try {
+                	youtube = crsdoc.select("iframe[width=510]").get(0).absUrl("src"); //Youtube link
+                } catch (Exception e) {
+                	youtube = "N/A";
+                }
+                	System.out.println("youtube: " + youtube); // DELETE ME
+                
                 Elements crsbodyele = crsdoc.select("section[class=course-detail clearfix]");
-                String CrsDes = "write your own code"; //Course Description Element
-                CrsDes = CrsDes.replace("'", "''");
-                CrsDes = CrsDes.replace(",", "");
-                if (CrsDes.contains("?")) {
-                    CrsDes = CrsDes.replace("?", "");
+	            
+                /** Course Description **/
+                String CrsDes;
+                try {
+                	CrsDes = crsdoc.select("div.readmore-container>div.full-body").text(); //Course Description Element
+	                CrsDes = CrsDes.replace("'", "''");
+	                CrsDes = CrsDes.replace(",", "");
+	                if (CrsDes.contains("?")) {
+	                    CrsDes = CrsDes.replace("?", "");
+	                }
+                } catch (Exception e) {
+                	CrsDes = "N/A";
                 }
-                	System.out.println("CrsDes: " + CrsDes); // DELETE ME
-                String Date = crsdoc.select("div[class=startdate]").text();
-                String StrDate = Date.substring(Date.indexOf(":") + 1, Date.length()); //Start date after the :
-                String datechk = StrDate.substring(0, StrDate.indexOf(" "));
-                if (!datechk.matches(".*\\d.*")) {
-                    if (StrDate.contains("n/a")) {
-                        StrDate = "write you own code";
-                    } else {
-                        StrDate = "write your own code";
-                    }
-                } else {
-                    String date = StrDate.substring(0, StrDate.indexOf(" "));
-                    String month = StrDate.substring(StrDate.indexOf(" ") + 1, StrDate.indexOf(" ") + 4);
-                    String year = StrDate.substring(StrDate.length() - 4, StrDate.length());
-                    StrDate = "write your own code";
+                System.out.println("CrsDes: " + CrsDes); // DELETE ME
+                
+                /** Start Date **/
+                java.sql.Date sqlStrDate;
+                java.util.Date dStrDate = new java.util.Date();                
+                try {
+                	String tempStrDate = crsdoc.select("h2.offering_dates_date").get(0).text(); // only get start dd/mm/yyyy
+                 		System.out.println("strDate: " + tempStrDate); // DELETE ME
+	                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	                dStrDate = sdf.parse(tempStrDate);
+	                sqlStrDate = new Date(dStrDate.getTime());
+	                //String strDate = sqlStrDate.toString().replace("-", "");
+                } catch (Exception e) {
+                	sqlStrDate = new java.sql.Date(0);
                 }
-                Element chk = crsdoc.select("div[class=effort last]").first();
-                Element crslenschk = crsdoc.select("div[class*=duration]").first();
-                String crsduration;
-                if (crslenschk == null) {
-                    crsduration = "0";
-                } else if (StrDate.contains("n/a self-paced")) {
-                    crsduration = "0";
-                } else {
-                    try {
-                        String crsdurationtmp = crsdoc.select("div[class*=duration]").text();
-                        int start = crsdurationtmp.indexOf(":") + 1;
-                        int end = crsdurationtmp.indexOf((" "), crsdurationtmp.indexOf(":"));
-                        crsduration = crsdurationtmp.substring(start, end);
-                    } catch (Exception e) {
-                        crsduration = "0";
-                        System.out.println("Exception");
-                    }
+                System.out.println("sqlStrDate: " + sqlStrDate); // DELETE ME
+                
+                /** Duration **/
+                int crsduration;
+                try {
+	                String tempEndDate = crsdoc.select("h2.offering_dates_date").get(1).text();
+	                	System.out.println("endDate: " + tempEndDate); // DELETE ME
+	                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	                java.util.Date dEndDate = sdf.parse(tempEndDate);
+	                long dateDiff = dEndDate.getTime() - dStrDate.getTime();
+	                crsduration = (int) TimeUnit.DAYS.convert(dateDiff, TimeUnit.MILLISECONDS);
+                } catch (Exception e) {
+                	crsduration = 0;
                 }
+                System.out.println("crsduration: " + crsduration); // DELETE ME
+                
                 //The following is used to insert scraped data into your database table. Need to uncomment all database related code to run this.
-                String query = "insert into course_data values(null,'" + CourseName + "','" + SCrsDesrpTemp + "','" + CrsDes + "','" + crsurl + "','" + youtube + "'," + StrDate + "," + crsduration + ",'" + CrsImg + "','','Edx')";
+                String query = "insert into course_data values(null,'" + CourseName + "','" + SCrsDesrpTemp + "','" + CrsDes + "','" + crsurl + "','" + youtube + "','" + sqlStrDate + "'," + crsduration + ",'" + CrsImg + "','','Open2Study')";
+                	System.out.println("query: " + query); // DELETE ME
                 System.out.println(query);
-                //statement.executeUpdate(query);// skip writing to database; focus on data printout to a text file instead.
-                //statement.close();
+                statement.executeUpdate(query);// skip writing to database; focus on data printout to a text file instead.
+                statement.close();
             }
         }
         connection.close();
     }
-
-//    public static void print(){
-//        BufferedWriter writer = null;
-//        try {
-//            //create a temporary file
-//            String timeLog = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-//            File logFile = new File(timeLog);
-//
-//            // This will output the full path where the file will be written to...
-//            System.out.println(logFile.getCanonicalPath());
-//
-//            writer = new BufferedWriter(new FileWriter(logFile,true));
-//
-//            for (Element e : ele) {
-//                writer.write(String.valueOf(e));
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                // Close the writer regardless of what happens...
-//                writer.close();
-//            } catch (Exception e) {
-//            }
-//        }
-//    }
 }
