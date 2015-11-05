ffe5e9388af7d3a630081c793863c1ee40fbbdf0
OwlRemember <byron.custodio@gmail.com>
2015-11-04 19:04:28 -0800

    Aggregated Search/Results
    Move folder to root of www folder and test using localhost/index.php

---
+++ OnCourse/error.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>OnCourse</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+</head>
+<body>
+	<div class="container-fluid">
+		<h3><p class="text-center">No search results :(</p></h3>
+		<a href="http://localhost/OnCourse/index.php"><p class="text-center">Go back to homepage.</p></a>
+	</div>
+</body>
+</html>
