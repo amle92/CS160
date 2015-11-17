3c96a3006577387556c00724a59f3f49de6595fd
amle92 <andyle1992@yahoo.com>
2015-11-17 14:29:42 -0800

    Added a mysqli_set_charset to get rid of the question marks
    +Added a mysqli_set_charset to get rid of the question marks when it is
    displayed on the web server.
    +Also noticed that there is a regular question mark in the short
    description of Pediatric Advanced Life Support course.

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 //require_once("connect.php");//provide a file that contains the database connection info
 /*change the credentials according to yours*/
-$conn = mysqli_connect("localhost", "youthcyb_160s2g4", "password", "test1");
+$conn = mysqli_connect("localhost", "youthcyb_160s2g4", "oncourse2015", "youthcyb_cs160s2g4");
+    mysqli_set_charset($conn, "utf8");      
+    if (!$conn) {
+        die("Database connection failed: " . mysqli_error());
+    } else {
+        $db_select = mysqli_select_db($conn, "youthcyb_cs160s2g4");
+        if (!$db_select) {
+            die("Database selection failed: " . mysqli_error());
+        }
+    }
+
 ?>
 <head>
 <title>OnCourse</title>
